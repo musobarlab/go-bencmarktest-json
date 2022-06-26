bca18056e0665e90862fac16b2033402445c7a37
wuriyanto48 <wuriyanto48@yahoo.co.id>
2022-06-26 13:34:34 +0700

    deserialize test: move serialize to loop

--- data_test.go
+++ data_test.go
@@ -51,49 +51,53 @@ func BenchmarkSerializeDataSnakeToJSON(b *testing.B) {
 }
 
 func BenchmarkDeserializeDataCamelFromJSON(b *testing.B) {
-	data := &DataCamel{
-		ID:                "1",
-		NameNameNome:      "Samsung Galaxy S10",
-		FieldOne:          "field one",
-		FieldTwo:          "field two",
-		FieldThree:        "field three",
-		FieldFour:         "field four",
-		FieldFive:         "field five",
-		FieldSix:          "field six",
-		FieldSeven:        "field seven",
-		FieldEight:        "field eight",
-		FieldNine:         "field nine",
-		FieldTen:          "field ten",
-		BlaBleBloQuantity: 10,
-		Images:            []string{"img1", "img2"},
-	}
 
-	rawData, _ := data.ToJSON()
 	for i := 0; i < b.N; i++ {
+		data := &DataCamel{
+			ID:                "1",
+			NameNameNome:      "Samsung Galaxy S10",
+			FieldOne:          "field one",
+			FieldTwo:          "field two",
+			FieldThree:        "field three",
+			FieldFour:         "field four",
+			FieldFive:         "field five",
+			FieldSix:          "field six",
+			FieldSeven:        "field seven",
+			FieldEight:        "field eight",
+			FieldNine:         "field nine",
+			FieldTen:          "field ten",
+			BlaBleBloQuantity: 10,
+			Images:            []string{"img1", "img2"},
+		}
+
+		rawData, _ := data.ToJSON()
+
 		FromCamelJSON(rawData)
 	}
 }
 
 func BenchmarkDeserializeDataSnakeFromJSON(b *testing.B) {
-	data := &DataSnake{
-		ID:                "1",
-		NameNameNome:      "Samsung Galaxy S10",
-		FieldOne:          "field one",
-		FieldTwo:          "field two",
-		FieldThree:        "field three",
-		FieldFour:         "field four",
-		FieldFive:         "field five",
-		FieldSix:          "field six",
-		FieldSeven:        "field seven",
-		FieldEight:        "field eight",
-		FieldNine:         "field nine",
-		FieldTen:          "field ten",
-		BlaBleBloQuantity: 10,
-		Images:            []string{"img1", "img2"},
-	}
 
-	rawData, _ := data.ToJSON()
 	for i := 0; i < b.N; i++ {
+		data := &DataSnake{
+			ID:                "1",
+			NameNameNome:      "Samsung Galaxy S10",
+			FieldOne:          "field one",
+			FieldTwo:          "field two",
+			FieldThree:        "field three",
+			FieldFour:         "field four",
+			FieldFive:         "field five",
+			FieldSix:          "field six",
+			FieldSeven:        "field seven",
+			FieldEight:        "field eight",
+			FieldNine:         "field nine",
+			FieldTen:          "field ten",
+			BlaBleBloQuantity: 10,
+			Images:            []string{"img1", "img2"},
+		}
+
+		rawData, _ := data.ToJSON()
+
 		FromSnakeJSON(rawData)
 	}
 }
